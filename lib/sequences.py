757bea14fe42251fdafea308ab54611c5747a1a4
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-27 11:10:50 +0300

    updates lib/sequence.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    fabinocci_sequence = [0,1]
+    while len(fabinocci_sequence) < length:
+        next_value = fabinocci_sequence[-1] + fabinocci_sequence[-2]
+        fabinocci_sequence.append(next_value)
+    
+    fabinocci_sequence =fabinocci_sequence[:length]
+    print(fabinocci_sequence)
+
+
